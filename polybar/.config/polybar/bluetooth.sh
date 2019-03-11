98739fe8e2b9c31b736a56a733e2bc78dd93f933
Jibreil <jibreilhoneine@gmail.com>
2019-03-11 22:44:03 +1100

    :crown:

--- polybar/.config/polybar/bluetooth.sh
+++ polybar/.config/polybar/bluetooth.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
-
 if [ "$(bluetoothctl -- show 14:AB:C5:70:C9:97 | grep -i "Powered")" = "	Powered: yes" ]; then
-	
-	if [ "$(bluetoothctl -- info 20:9B:A5:92:CC:61 | grep -i "Connected")" = "	Connected: yes" ]; then
-	
-		echo "  "
-
-	else
-
-		echo " "
-
-	fi
-
+  if [ "$(bluetoothctl -- info 20:9B:A5:92:CC:61 | grep -i "Connected")" = "	Connected: yes" ]; then
+    echo "  "
+  else
+    echo " "
+  fi
 else
-	echo ""
+  echo ""
 fi
